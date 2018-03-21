b8aa4d83f05b6311fad2eb925daa81eb30f0e47a
MSiccDev <msiccdev@hotmail.com>
2018-03-21 13:29:41 +0100

    added MaxLinesLabel #XfQaD

---
+++ MaxLines/MaxLinesLabel/MaxLinesLabel/MaxLinesLabel.Android/XfMaxLinesLabelRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Text;
+using MaxLinesLabel;
+using MaxLinesLabel.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(XfMaxLinesLabel), typeof(XfMaxLinesLabelRenderer))]
+namespace MaxLinesLabel.Droid
+{
+    class XfMaxLinesLabelRenderer : LabelRenderer
+    {
+        public XfMaxLinesLabelRenderer(Context context) : base(context)
+        {
+        }
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (((XfMaxLinesLabel)e.NewElement).MaxLines == -1 || ((XfMaxLinesLabel)e.NewElement).MaxLines == int.MaxValue)
+                return;
+            this.Control.SetMaxLines(((XfMaxLinesLabel)e.NewElement).MaxLines);
+            this.Control.Ellipsize = TextUtils.TruncateAt.End;
+        }
+
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == XfMaxLinesLabel.MaxLinesProperty.PropertyName)
+            {
+                if (((XfMaxLinesLabel)this.Element).MaxLines == -1 || ((XfMaxLinesLabel)this.Element).MaxLines == int.MaxValue)
+                    return;
+                this.Control.SetMaxLines(((XfMaxLinesLabel)this.Element).MaxLines);
+                this.Control.Ellipsize = TextUtils.TruncateAt.End;
+            }
+        }
+    }
+}
